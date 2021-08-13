e56e13f9bcd9b206d1822259056d06aeff26f2a1
father <87602534+Weeraphon41@users.noreply.github.com>
2021-08-13 23:05:27 +0700

    8/13/64

--- resources/views/questionnaire/quest13.blade.php
+++ resources/views/questionnaire/quest13.blade.php
@@ -10,82 +10,82 @@
 <tbody>
     <tr>
         <td>นอนไม่เพียงพอ (เพราะผู้พิการตื่นบ่อยหรือร้องขอความช่วยเหลือฯลฯ)</td>
-        <td><input id="qt13_s1_rd1" type="radio" name="quest13" value="rd1"></td>
-        <td><input id="qt13_s1_rd0" type="radio" name="quest13" value="rd0"></td>
+        <td><input id="qt13_s1_rd1" type="radio" name="quest13_1" value="rd1"></td>
+        <td><input id="qt13_s1_rd0" type="radio" name="quest13_1" value="rd0"></td>
     </tr>
   <tr>
     
     <td>ไม่สะดวกในการดูแลผู้พิการ (เพราะการดูแลผู้พิการติดเตียงต้องใช้เวลามาก
 หรือต้องเดินทาง ฯลฯ)</td>
-    <td>0</td>
-    <td>0</td>
+    <td><input id="qt13_s2_rd1" type="radio" name="quest13_2" value="rd1"></td>
+    <td><input id="qt13_s2_rd0" type="radio" name="quest13_2" value="rd0"></td>
   </tr>
   <tr>
     <td>รู้สึกมีปัญหาทางกาย (เพราะต้องช่วยเหลือผู้พิการในการเคลื่อนย้าย และต้อง
 ใช้ความพยายามในการดูแลเอาใจใส่อย่างมาก)
 </td>
-    <td>0</td>
-    <td>0</td>
+    <td><input id="qt13_s3_rd1" type="radio" name="quest13_3" value="rd1"></td>
+    <td><input id="qt13_s3_rd0" type="radio" name="quest13_3" value="rd0"></td>
   </tr>
   <tr>
     <td>การดูแลผู้พิการทำให้ท่านไม่มีเวลาทำอย่างอื่น
 </td>
-    <td>0</td>
-    <td>0</td>
+    <td><input id="qt13_s4_rd1" type="radio" name="quest13_4" value="rd1"></td>
+    <td><input id="qt13_s4_rd0" type="radio" name="quest13_4" value="rd0"></td>
   </tr>
   <tr>
     <td>การดูแลผู้พิการทำให้คนในครอบครัวต้องปรับตัว (เพราะการดูแลผู้พิการทำให้เกิดการเปลี่ยนแปลงเวลาการทำงานหรือกิจวัตรประจำวัน/หรือทำให้ไม่มีเวลาเป็นส่วนตัว)
 </td>
-    <td>0</td>
-    <td>0</td>
+    <td><input id="qt13_s5_rd1" type="radio" name="quest13_5" value="rd1"></td>
+    <td><input id="qt13_s5_rd0" type="radio" name="quest13_5" value="rd0"></td>
   </tr>
   <tr>
     <td>การดูแลผู้พิการทำให้ท่านต้องเปลี่ยนแผนการทำงานของท่าน (เช่นต้องปฏิเสธงานที่ได้รับมอบหมาย หรือไม่สามารถไปเที่ยวหรือทำธุระส่วนตัวได้)
 </td>
-    <td>0</td>
-    <td>0</td>
+    <td><input id="qt13_s6_rd1" type="radio" name="quest13_6" value="rd1"></td>
+    <td><input id="qt13_s6_rd0" type="radio" name="quest13_6" value="rd0"></td>
   </tr>
   <tr>
     <td>ต้องให้เวลากับคนในครอบครัวมากขึ้น
 </td>
-    <td>0</td>
-    <td>0</td>
+    <td><input id="qt13_s7_rd1" type="radio" name="quest13_7" value="rd1"></td>
+    <td><input id="qt13_s7_rd0" type="radio" name="quest13_7" value="rd0"></td>
   </tr>
   <tr>
     <td>ต้องพยายามปรับตัวหรือสภาพจิตใจและอารมณ์ (เพราะมักมีการขัดแย้งรุนแรงเกิดขึ้น)
 </td>
-    <td>0</td>
-    <td>0</td>
+    <td><input id="qt13_s8_rd1" type="radio" name="quest13_8" value="rd1"></td>
+    <td><input id="qt13_s8_rd0" type="radio" name="quest13_8" value="rd0"></td>
   </tr>
   <tr>
     <td>อาการหรือพฤติกรรมบางอย่างของผู้พิการทำให้ท่านวิตกกังวลหรือหนักใจ(เช่น ไม่สามารถควบคุมการขับถ่าย มีแผลกดทับ ฯลฯ)
 </td>
-    <td>0</td>
-    <td>0</td>
+    <td><input id="qt13_s9_rd1" type="radio" name="quest13_9" value="rd1"></td>
+    <td><input id="qt13_s9_rd0" type="radio" name="quest13_9" value="rd0"></td>
   </tr>
   <tr>
     <td>รู้สึกไม่สบายใจที่ผู้พิการด้านร่างกายไม่สามารถช่วยเหลือตนเองได้
 </td>
-    <td>0</td>
-    <td>0</td>
+    <td><input id="qt13_s10_rd1" type="radio" name="quest13_10" value="rd1"></td>
+    <td><input id="qt13_s10_rd0" type="radio" name="quest13_10" value="rd0"></td>
   </tr>
   <tr>
     <td>ต้องมีการปรับตัวเรื่องการทำงาน (เช่นต้องลางานเพื่อมาดูแลผู้พิการบ่อย ๆ)
 </td>
-    <td>0</td>
-    <td>0</td>
+    <td><input id="qt13_s11_rd1" type="radio" name="quest13_11" value="rd1"></td>
+    <td><input id="qt13_s11_rd0" type="radio" name="quest13_11" value="rd0"></td>
   </tr>
   <tr>
     <td>เกิดปัญหาด้านการเงิน
 </td>
-    <td>0</td>
-    <td>0</td>
+    <td><input id="qt13_s12_rd1" type="radio" name="quest13_12" value="rd1"></td>
+    <td><input id="qt13_s12_rd0" type="radio" name="quest13_12" value="rd0"></td>
   </tr>
   <tr>
     <td>รู้สึกว่ามีปัญหารุมเร้า (เช่นท่านกังวลตลอดเวลาว่าจะแก้ปัญหาที่รุมเร้าได้อย่างไร)
 </td>
-    <td>0</td>
-    <td>0</td>
+    <td><input id="qt13_s13_rd1" type="radio" name="quest13_13" value="rd1"></td>
+    <td><input id="qt13_s13_rd0" type="radio" name="quest13_13" value="rd0"></td>
   </tr>
 
 
